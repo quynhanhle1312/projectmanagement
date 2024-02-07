6e32afbbbde8f604e41de7b57e5ae20ffeeda38d
Quynh Anh Le <156391997+quynhanhle1312@users.noreply.github.com>
2024-02-07 14:55:25 +0700

    Add files via upload

---
+++ projects/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'projects'
+
+urlpatterns = [
+    path('', views.projects, name='projects'),
+    path('new-project/', views.newProject, name='new-project'),
+    path('new-task/', views.newTask, name='new-task'),
+]
